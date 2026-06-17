ba5c47b2ae125632aea49c43f34c9130347d34e5
zed frost <zed@zeddeMacBook-Air.local>
2026-06-17 16:29:54 +0800

    update

--- script/analysis_subset.sh
+++ script/analysis_subset.sh
@@ -1,4 +1,4 @@
 python /dataset-2041446603085574144/Riskscope/utils/evaluate_subset.py \
   --subset "/dataset-2041446603085574144/Riskscope/outputs/random_subsets/subset_${1}.jsonl" \
-  --infer /dataset-2041446603085574144/Riskscope/model/v7-20260607-003145/checkpoint-2612/infer_result/infer_${1}.jsonl \
+  --infer /dataset-2041446603085574144/Riskscope/outputs/random_subsets/infer_${1}.jsonl \
   --output "/dataset-2041446603085574144/Riskscope/outputs/random_subsets/subset_${1}_report.json"
